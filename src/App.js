42b4ea5a073aeb88e00a3d87a010e09ec397c7f2
Wendel Macedo <wendel@MacBook-Pro-de-Wendel.local>
2021-06-01 16:27:21 -0300

    Create react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Container, Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <Container>
+      <Typography>Teste 123</Typography>
+    </Container>
+  );
+}
+
+export default App;
