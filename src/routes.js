6bc0699b92c27081bc4aec2341deedd9fbbf92de
RonaldoCaetano <ronaldogodoi2@gmail.com>
2021-06-08 12:12:22 -0300

    adicionando as rotas

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "screens/Dashboard/Dashboard";
+import PageNotFound from "screens/404/404";
+import Login from "screens/Login/Login";
+import UserRegistration from "screens/User/UserRegistration";
+import BrandRegistration from "screens/Brand/BrandRegistration";
+import UserForgotPassword from "screens/User/UserForgotPassword";
+import ListUsers from "screens/User/ListUser";
+import ListBrand from "screens/Brand/ListBrand";
+import VehicleRegistration from "screens/Vehicle/VehicleRegistration";
+import ListVehicle from "screens/Vehicle/ListVehicle";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/inicio" component={Dashboard} />
+        <Route path="usuarios/cadastro" component={UserRegistration} />
+        <Route path="/usuarios" component={ListUsers} />
+        <Route path="/marcas/cadastro" component={BrandRegistration} />
+        <Route path="/marcas" component={ListBrand} />
+        <Route path="/veiculos/cadastro" component={VehicleRegistration} />
+        <Route path="/veiculos" component={ListVehicle} />
+        <Route path="/recover" component={UserForgotPassword} />
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
---
+++ src/screens/404/404.js
@@ -0,0 +1,8 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const NotFound = () => {
+  return <Typography>Não encontramos sua página</Typography>;
+};
+
+export default NotFound;
