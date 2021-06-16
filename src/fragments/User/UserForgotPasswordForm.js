39e859a29f62188697bd07c72210e7db3d51e38a
RonaldoCaetano <ronaldogodoi2@gmail.com>
2021-06-16 15:32:40 -0300

    formatando alguns arquivos

--- src/fragments/User/UserForgotPasswordForm.js
+++ src/fragments/User/UserForgotPasswordForm.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { GridFullHeight } from "components/GridFullHeight/GridFullHeight";
 import TextInput from "components/TextInput/TextInput";
-import { Button, Typography} from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 
 const UserForgotPasswordForm = () => {
   return (
-      <GridFullHeight
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-      >
-        <form>
-            <Typography variant="h5">Esqueceu a senha?</Typography>
-            <Typography variant="subtitle1">Informe seu e-mail ou login e enviaremos instruções para você criar sua senha.</Typography>
-            <TextInput id="username" required label="E-mail / Login" />
-        </form>
-    
-        <div>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            margin="normal"
-          >
-            Enviar
-          </Button>
-        </div>
-      </GridFullHeight>
+    <GridFullHeight
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <form>
+        <Typography variant="h5">Esqueceu a senha?</Typography>
+        <Typography variant="subtitle1">
+          Informe seu e-mail ou login e enviaremos instruções para você criar
+          sua senha.
+        </Typography>
+        <TextInput id="username" required label="E-mail / Login" />
+      </form>
+
+      <div>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          margin="normal"
+        >
+          Enviar
+        </Button>
+      </div>
+    </GridFullHeight>
   );
 };
 
--- src/screens/Vehicle/ListVehicle.js
+++ src/screens/Vehicle/ListVehicle.js
@@ -1,13 +1,14 @@
-import React from "react"
+import React from "react";
 const { GridFullHeight } = require("components/GridFullHeight/GridFullHeight");
-const { default: ListVehicleTable } = require("fragments/Vehicle/ListVehicleTable");
+const {
+  default: ListVehicleTable,
+} = require("fragments/Vehicle/ListVehicleTable");
 
-const ListVehicle = () =>{
-
-    return(
-        <GridFullHeight>
-            <ListVehicleTable />
-        </GridFullHeight>
-    );
-}
+const ListVehicle = () => {
+  return (
+    <GridFullHeight>
+      <ListVehicleTable />
+    </GridFullHeight>
+  );
+};
 export default ListVehicle;
