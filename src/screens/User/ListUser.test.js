7e63bb9ebdd3cb142730600d4544538a468c1a13
RonaldoCaetano <ronaldogodoi2@gmail.com>
2021-06-17 17:53:32 -0300

    ajustando import

--- src/screens/User/ListUser.test.js
+++ src/screens/User/ListUser.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import LoadingProvider from 'contexts/LoadingContext';
-import ListUser from './ListUser';
+import ListUser from './ListUsers';
 
 describe('<ListUser', () => {
   it('should return a table', () => {
