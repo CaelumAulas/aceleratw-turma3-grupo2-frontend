3ea94c8d6475a28ba004d939463c20634e807811
RonaldoCaetano <ronaldogodoi2@gmail.com>
2021-06-18 14:59:21 -0300

    adding content-type

--- src/api/config.js
+++ src/api/config.js
@@ -3,4 +3,5 @@ export const BEARER_TOKEN = localStorage.getItem('Token');
 export const HEADERS = {
   Accept: 'application/json',
   Authorization: `Bearer ${BEARER_TOKEN}`,
+  'Content-Type': 'application/json',
 };
